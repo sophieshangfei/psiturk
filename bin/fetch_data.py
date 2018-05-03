db182afd96ed93787073e14e417d68bd72b6dbf2
fredcallaway <fredc@llaway.com>
2018-05-02 17:54:17 -0700

    oops, ran more in version 0.1!

--- bin/fetch_data.py
+++ bin/fetch_data.py
@@ -153,7 +153,9 @@ def reformat_data(version):
     # Split tdf into separate dataframes for each type of trial.
     data = {'participants': pdf}
     for trial_type, df in tdf.groupby('trial_type'):
-        df = df.dropna(axis=1)
+        x = ~df.isnull().all()
+        keep = x.index[x].tolist()
+        df = df[keep]
         df = df.drop('internal_node_id', axis=1)
         df = df.drop('trial_index', axis=1)
         df.columns = [to_snake_case(c) for c in df.columns]
